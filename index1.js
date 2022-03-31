a48df1e3ef85413dab935a5f0552f715ed9bb3ac
prem sai vittal <premsai2030@gmail.com>
2022-04-01 00:54:59 +0530

    added comments and formatted the code

--- index1.js
+++ index1.js
@@ -1,36 +1,37 @@
 
-var currentMonth=0;
+var month = 0;
+var year = 0;
 const calendar = document.getElementById('calender');
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     date = new Date();
-    var year = date.getFullYear();
-    var month = date.getMonth();
-    loadCalender(year,month)
-    loadYears(year,month);
-    
+    year = date.getFullYear();
+    month = date.getMonth();
+    loadCalender(year, month)
+    loadYears(year, month);
+
 })
 
-function loadCalender(year,month){
-
+function loadCalender(year, month) {
     const firstDayOfMonth = new Date(year, month, 1);
-    const CurrMonthDays = new Date(year, month + 1, 0).getDate();
-    const preMonthDays = new Date(year, month, 0).getDate(); 
+    const CurrMonthDays = new Date(year, month + 1, 0).getDate(); // use currentMonthDays not Curr..
+    const preMonthDays = new Date(year, month, 0).getDate();
     //const nextMonthDay = new Date(year ,month+2 ,0).getDate();  
-    const currMonth = new Date(year,month+1,0);
-    const nextMonth =new Date(year,month+2,0);
-   
-    document.getElementById('Show').innerText = 
+    const currMonth = new Date(year, month + 1, 0);
+    const nextMonth = new Date(year, month + 2, 0);
+
+    // dont use template strings
+    document.getElementById('Show').innerText =
         `${currMonth.toLocaleDateString('en-us', { month: 'long' })} `;
 
-    let blankDays = (firstDayOfMonth.getDay() - 1 > 0) ? (firstDayOfMonth.getDay() - 1) :  6 ;
-   
-    preBlank = blankDays;
-   var count = (blankDays == 6 || (blankDays == 5 && CurrMonthDays == 31) ) ? 42 : 35 ;
-    
-    calendar.innerHTML='';
+    let blankDays = (firstDayOfMonth.getDay() - 1 > 0) ? (firstDayOfMonth.getDay() - 1) : 6;
 
-    for(var i =1 ; i<= (count ) ; i++){
+    preBlank = blankDays;
+    var count = (blankDays == 6 || (blankDays == 5 && CurrMonthDays == 31)) ? 42 : 35;
+
+    calendar.innerHTML = '';
+
+    for (var i = 1; i <= (count); i++) {
         preBlank--;
         if (i <= blankDays) {
             const PreCalenDays = document.createElement('div');
@@ -41,24 +42,24 @@ function loadCalender(year,month){
             calendar.appendChild(PreCalenDays);
         }
 
-        if(i > blankDays && i <= CurrMonthDays+blankDays){
-            
+        if (i > blankDays && i <= CurrMonthDays + blankDays) {
+
             const CalenDays = document.createElement('div');
             CalenDays.classList.add('day');
             var renderDays = i - blankDays;
-           // console.log(i ,blankDays,renderDays);
+            // console.log(i ,blankDays,renderDays);
             CalenDays.innerText = renderDays;
             CalenDays.style["color"] = 'blue';
             calendar.appendChild(CalenDays);
         }
 
-        if(i>CurrMonthDays+blankDays && i<= count){
+        if (i > CurrMonthDays + blankDays && i <= count) {
             const NextCalenDays = document.createElement('div');
             NextCalenDays.classList.add('day');
             var renderDays = i - (blankDays + CurrMonthDays);
             NextCalenDays.innerText = renderDays;
             if (renderDays == 1) {
-             renderDays = '1 ' + `${nextMonth.toLocaleDateString('en-us', { month: 'long' })}`;
+                renderDays = '1 ' + `${nextMonth.toLocaleDateString('en-us', { month: 'long' })}`;
             }
             NextCalenDays.innerText = renderDays;
             calendar.appendChild(NextCalenDays);
@@ -70,33 +71,32 @@ function loadCalender(year,month){
 
 
 
-function loadYears(year,month){
-  
-Saal = document.getElementById('year');
+function loadYears() {
+
+    Saal = document.getElementById('year');
 
     const startYear = 1900;
     const endYear = 2040;
 
-    for (var i = startYear, j = 0 ; i <= endYear; i++, j++) {
+    for (var i = startYear, j = 0; i <= endYear; i++, j++) {
 
         const Addoption = document.createElement("option");
         Addoption.value = i;
         Addoption.innerHTML = i;
         Saal.options.add(Addoption);
-    
-       Saal.options[j].removeAttribute("selected", "selected");
+
+        Saal.options[j].removeAttribute("selected", "selected");
         if (i == year) {
-         
+
             Saal.options[j].setAttribute("selected", "selected");
 
         }
     }
     Saal.onchange = () => {
-    
-        var selectedYear = Saal.value;
-        console.log(selectedYear,month);
-        loadCalender(selectedYear,month+currentMonth);
-    
+
+        year = Saal.value;
+        loadCalender(year, month);
+
     }
 
 }
@@ -104,19 +104,15 @@ Saal = document.getElementById('year');
 function changeMonthByArrow() {
 
     document.getElementById('nextButton').addEventListener('click', () => {
-        currentMonth++;
-        month =date.getMonth();
-        year = date.getFullYear();
-        loadCalender(year,month+currentMonth);   
+        month++;
+        loadCalender(year, month);
 
     });
 
     document.getElementById('backButton').addEventListener('click', () => {
-        currentMonth--;
-        month =date.getMonth();
-        year = date.getFullYear();  
-        loadCalender(year , month+currentMonth);
-        
+        month--;
+        loadCalender(year, month);
+
     });
 
 }
